023abc60e686e6ac4721c561c713276181f9947f
pawelpolak88 <nikita.taranaw@gmail.com>
2022-03-25 15:53:40 +0400

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,26 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+// document.addEventListener("DOMContentLoaded", () => {});
 
-document.addEventListener('DOMContentLoaded', () => {
+// Табы для меню
 
-	// Custom JS
+document.querySelectorAll(".tabs-triggers__item").forEach((item) => {
+	item.addEventListener("click", (e) => {
+		e.preventDefault();
 
-})
+		const id = e.target.getAttribute("href").replace("#", "");
+
+		document
+			.querySelectorAll(".tabs-triggers__item")
+			.forEach((child) =>
+				child.classList.remove("tabs-triggers__item--active")
+			);
+
+		document
+			.querySelectorAll(".block")
+			.forEach((child) => child.classList.remove("block-active"));
+
+		item.classList.add("tabs-triggers__item--active");
+		document.getElementById(id).classList.add("block-active");
+	});
+});
+
+document.querySelector(".tabs-triggers__item").click();

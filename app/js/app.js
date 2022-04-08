56111229c891b85dde3fcd74104ef90bbc23759e
pawelpolak88 <nikita.taranaw@gmail.com>
2022-04-08 10:56:36 +0400

    new version

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,89 @@
-// document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
 
-// Табы для меню
+	// Tabs menu
 
-document.querySelectorAll(".tabs-triggers__item").forEach((item) => {
-	item.addEventListener("click", (e) => {
-		e.preventDefault();
+	const getId = (link) => link.getAttribute("href").replace("#", "");
 
-		const id = e.target.getAttribute("href").replace("#", "");
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					document.querySelectorAll(".nav-list__item-link").forEach((link) => {
+						link.classList.toggle(
+							"nav-list__item-link--active",
+							getId(link) === entry.target.id
+						);
+					});
+				}
 
-		document
-			.querySelectorAll(".tabs-triggers__item")
-			.forEach((child) =>
-				child.classList.remove("tabs-triggers__item--active")
-			);
 
-		document
-			.querySelectorAll(".block")
-			.forEach((child) => child.classList.remove("block-active"));
+			});
+		},
+		{
+			threshold: 0.7,
+		}
+	);
 
-		item.classList.add("tabs-triggers__item--active");
-		document.getElementById(id).classList.add("block-active");
+	document.querySelectorAll(".block").forEach((block) => observer.observe(block));
+
+	document.querySelector(".nav-list").addEventListener("click", (event) => {
+		if (event.target.classList.contains("nav-list__item-link")) {
+			event.preventDefault();
+
+			window.scrollTo({
+				top: document.getElementById(getId(event.target)).offsetTop,
+				behavior: "smooth",
+			});
+		}
 	});
+
+	// Burger menu
+
+	let menuBtn = document.querySelector(".menu-btn");
+	let menu = document.querySelector(".menu");
+
+	menuBtn.addEventListener("click", function () {
+		document.body.classList.toggle("lock");
+		menuBtn.classList.toggle("active");
+		menu.classList.toggle("active");
+	});
+
+
+	const menuLinks = document.querySelectorAll(
+		".burger-list__item-link[data-goto]"
+	);
+
+	if (menuLinks.length > 0) {
+		menuLinks.forEach((menuLink) => {
+			menuLink.addEventListener("click", onMenuLinkClick);
+		});
+
+		function onMenuLinkClick(e) {
+			const menuLink = e.target;
+
+			if (
+				menuLink.dataset.goto &&
+				document.querySelector(menuLink.dataset.goto)
+			) {
+				const gotoBlock = document.querySelector(menuLink.dataset.goto);
+
+				const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+
+				if (menuBtn.classList.contains("active")) {
+					document.body.classList.remove("lock");
+					menuBtn.classList.remove("active");
+					menu.classList.remove("active");
+				}
+
+				window.scrollTo({
+					top: gotoBlockValue,
+					behavior: "smooth",
+				});
+
+				e.preventDefault();
+			}
+		}
+	}
+
 });
 
-document.querySelector(".tabs-triggers__item").click();
